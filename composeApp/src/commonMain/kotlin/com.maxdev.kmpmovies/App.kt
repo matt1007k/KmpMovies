ea550b71a45fdbdec431ca334933cf1c5ef9dbcc
Max Meza <matt1007k@gmail.com>
2024-06-28 00:17:56 -0500

    Create project, settings initial and Home screen

---
+++ composeApp/src/commonMain/kotlin/com.maxdev.kmpmovies/App.kt
@@ -0,0 +1,24 @@
+package com.maxdev.kmpmovies
+
+import androidx.compose.runtime.Composable
+import coil3.ImageLoader
+import coil3.annotation.ExperimentalCoilApi
+import coil3.compose.setSingletonImageLoaderFactory
+import coil3.request.crossfade
+import coil3.util.DebugLogger
+import com.maxdev.kmpmovies.ui.screens.home.HomeScreen
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+@Preview
+fun App() {
+    setSingletonImageLoaderFactory { context ->
+        ImageLoader.Builder(context)
+            .crossfade(true)
+            .logger(DebugLogger())
+            .build()
+    }
+    HomeScreen()
+}
+
---
+++ composeApp/src/iosMain/kotlin/com.maxdev.kmpmovies/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.maxdev.kmpmovies
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
